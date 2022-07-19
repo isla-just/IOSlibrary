b2cd184230375f77851ee6eb037cbe682662eb7f
isla-just <200080@virtualwindow.co.za>
2022-07-19 11:44:15 +0200

    initial file upload

---
+++ OISLibrary/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Isla Just's Macbook Pro on 2022/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OISLibrary/Shared/OISLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OISLibraryApp.swift
+//  Shared
+//
+//  Created by Isla Just's Macbook Pro on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct OISLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
